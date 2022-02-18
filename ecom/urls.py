acf58212894b8bd1c13f32aa8903ce1e0ba10f75
yashhtyagi <tyagi630@gmail.com>
2022-02-18 17:16:43 +0530

    creating api

--- ecom/urls.py
+++ ecom/urls.py
@@ -1,4 +1,3 @@
-from xml.dom.minidom import Document
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static 
@@ -7,6 +6,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/', include('api.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
