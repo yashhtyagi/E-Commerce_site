c0c4de2ed2b14842eb4a8f349899e17f7e16b78b
yashhtyagi <tyagiy630@gmail.com>
2022-03-04 11:46:48 +0530

    category API

---
+++ api/category/serializers.py
@@ -0,0 +1,10 @@
+from dataclasses import field
+from pyexpat import model
+from rest_framework import serializers
+from .models import Category
+
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('name', 'discription')
+    
--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
+from unicodedata import category
 from django.urls import path, include
 from rest_framework.authtoken import views
 from .views import home
 
 urlpatterns = [
-    path('', home, name='api.home')
+    path('', home, name='api.home'),
+    path('category/', include('api.category.urls'))
 ]
